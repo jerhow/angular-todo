8d4c3e52d11d6173f8d6c91f0cfdb99f36b42006
Jerry Howard <jerryhoward@fastmail.fm>
2025-08-05 17:02:47 -0600

    Display the list and the input form

--- my-todo-app/src/app/app.component.ts
+++ my-todo-app/src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+// 1. IMPORT the component you want to use
+import { TodoComponent } from './todo/todo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, // This property marks it as a standalone component
+  // 2. ADD the component to the 'imports' array
+  imports: [CommonModule, RouterOutlet, TodoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
